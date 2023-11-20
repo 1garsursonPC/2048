1e122217aaed5bb31f908abc63a45a815192b949
Kortans <kortansyolo@gmail.com>
2023-11-20 10:23:50 +0100

    Inclusion de Grille.h et rectification de l'appel de l'enum

--- Grille.cpp
+++ Grille.cpp
@@ -1,1 +1,25 @@
 #pragma once
+#include "Grille.h"
+
+class Grille {
+    public:
+        Grille(){
+
+            
+
+        }
+
+        void changer_grille(EntreUtilisateur);
+        //mettre le tableau de la grille en type pour getGrille
+        unsigned getGrille();
+        unsigned getScore();
+
+        friend std::ostream& operator << (std::ostream& os, Grille _grille);
+
+    private:
+        std::array <std::array<unsigned,4>,4>_grille;
+        void deplacer();
+        void fusionner();
+        void ajout_2();
+
+};
